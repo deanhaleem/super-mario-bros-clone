1196b52a5071f572eba38b52a09313358d1a3419
Dean Haleem <deanhaleem.10@gmail.com>
2022-11-11 14:37:51 -0500

    initial game scene and repo setup

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSameLine: true,
+  endOfLine: 'auto'
+}
+  
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import Phaser from 'phaser';
+import GameScene from './scenes/Game';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  backgroundColor: '#33A5E7',
+  scale: {
+    width: 800,
+    height: 600,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
+
+new Phaser.Game(
+  Object.assign(config, {
+    scene: [GameScene],
+  })
+);
